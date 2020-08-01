03462ab5dda88e1cd5d30f297fb1583c6d289101
Anand Gupta <aanand.gupta03@gmail.com>
2020-08-02 00:20:36 +0530

    Added dynamic media query

--- constants/app.constants.js
+++ constants/app.constants.js
@@ -1,11 +1,11 @@
 const CARDS_DATA = [
-    { value: 1, color: '#BFBFBF' },
-    { value: 2, color: '#2B8EAD' },
-    { value: 3, color: '#333333' },
-    { value: 4, color: '#6F98A8' },
-    { value: 5, color: '#CCCCCC' },
-    { value: 6, color: '#000000' },
-    { value: 7, color: '#72C3DC' },
+    { value: 1, color: '#333333' },
+    { value: 2, color: '#EFEFEF' },
+    { value: 3, color: '#72C3DC' },
+    { value: 4, color: '#2B8EAD' },
+    { value: 5, color: '#6F98A8' },
+    { value: 6, color: '#BFBFBF' },
+    { value: 7, color: '#2F454E' },
     { value: 8, color: '#6fc1f3' },
     { value: 9, color: '#2F454E' },
   ];
